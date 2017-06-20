5353f91c10ac2635a3b850cdff1e15e8e68a4007
mnshr <mhira@alumni.nd.edu>
2017-06-19 23:16:12 -0400

    updated

--- read_write.py
+++ read_write.py
@@ -7,5 +7,13 @@ Created on Sat Jun 17 22:54:57 2017
 
 import pandas as pd
 
+data_file ='abc_data.csv'
+
+# Read from a json file
 train_df = pd.read_json("../input/train.json")
 train_df.head()
+
+
+# Dump to a json file
+json_df = pd.read_csv(data_file)
+json_df.to_json('./data.json',orient='index')
