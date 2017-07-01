1be83fe361ca647de5fe0d47e40f7192e7d0cc5a
mnshr <mhira@alumni.nd.edu>
2017-06-30 23:54:47 -0400

    updated

--- read_write.py
+++ read_write.py
@@ -17,3 +17,10 @@ train_df.head()
 # Dump to a json file
 json_df = pd.read_csv(data_file)
 json_df.to_json('./data.json',orient='index')
+
+
+#https://www.kaggle.com/fhoffa/strata-london-2017-ratings-1st-exploration
+# Read in CSV to dataframe
+df = pd.read_csv('../input/Strata London 2017 ratings - results-20170630-170827.csv.csv')
+df['responses'] = df['responses'].fillna(0).astype(int)
+df['starthour'] = df['starthour'].fillna(np.nan).astype(int)
