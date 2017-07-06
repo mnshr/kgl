83fe13f529fc40743d46dd76ec80e63f056581f5
mnshr <mhira@alumni.nd.edu>
2017-07-05 22:04:18 -0400

    updated xgb

--- ml-models.py
+++ ml-models.py
@@ -29,3 +29,103 @@ ranks["Lasso"] = ranking(np.abs(lasso.coef_), colnames)
 rf = RandomForestRegressor(n_jobs=-1, n_estimators=50, verbose=3)
 rf.fit(X,Y)
 ranks["RF"] = ranking(rf.feature_importances_, colnames)
+
+
+#GBM
+#https://www.kaggle.com/aharless/xgb-w-o-outliers-lgb-with-outliers-combined/code
+# Parameters
+lgb_weight = 0.2
+
+
+import numpy as np
+import pandas as pd
+import xgboost as xgb
+from sklearn.preprocessing import LabelEncoder
+import lightgbm as lgb
+import gc
+
+
+##### READ IN RAW DATA
+
+print( "\nReading data from disk ...")
+prop = pd.read_csv('../input/properties_2016.csv')
+train = pd.read_csv("../input/train_2016_v2.csv")
+
+
+
+##### PROCESS DATA FOR LIGHTGBM
+
+print( "\nProcessing data for LightGBM ..." )
+for c, dtype in zip(prop.columns, prop.dtypes):
+    if dtype == np.float64:
+        prop[c] = prop[c].astype(np.float32)
+
+df_train = train.merge(prop, how='left', on='parcelid')
+
+x_train = df_train.drop(['parcelid', 'logerror', 'transactiondate', 'propertyzoningdesc', 'propertycountylandusecode'], axis=1)
+y_train = df_train['logerror'].values
+print(x_train.shape, y_train.shape)
+
+train_columns = x_train.columns
+
+for c in x_train.dtypes[x_train.dtypes == object].index.values:
+    x_train[c] = (x_train[c] == True)
+
+del df_train; gc.collect()
+
+split = 90000
+x_train, y_train, x_valid, y_valid = x_train[:split], y_train[:split], x_train[split:], y_train[split:]
+x_train = x_train.values.astype(np.float32, copy=False)
+x_valid = x_valid.values.astype(np.float32, copy=False)
+
+d_train = lgb.Dataset(x_train, label=y_train)
+d_valid = lgb.Dataset(x_valid, label=y_valid)
+
+
+
+##### RUN LIGHTGBM
+#https://www.kaggle.com/aharless/xgb-w-o-outliers-lgb-with-outliers-combined/code
+import xgboost as xgb
+from sklearn.preprocessing import LabelEncoder
+import lightgbm as lgb
+import gc
+
+params = {}
+params['max_bin'] = 10
+params['learning_rate'] = 0.0021 # shrinkage_rate
+params['boosting_type'] = 'gbdt'
+params['objective'] = 'regression'
+params['metric'] = 'l1'          # or 'mae'
+params['sub_feature'] = 0.5      # feature_fraction -- OK, back to .5, but maybe later increase this
+params['bagging_fraction'] = 0.85 # sub_row
+params['bagging_freq'] = 40
+params['num_leaves'] = 512        # num_leaf
+params['min_data'] = 500         # min_data_in_leaf
+params['min_hessian'] = 0.05     # min_sum_hessian_in_leaf
+
+watchlist = [d_valid]
+print("\nFitting LightGBM model ...")
+clf = lgb.train(params, d_train, 430, watchlist)
+
+# xgboost params
+xgb_params = {
+    'eta': 0.038,
+    'max_depth': 5,
+    'subsample': 0.80,
+    'objective': 'reg:linear',
+    'eval_metric': 'mae',
+    'lambda': 0.9,
+    'alpha': 0.3,
+    'base_score': y_mean,
+    'silent': 1
+}
+dtrain = xgb.DMatrix(x_train, y_train)
+dtest = xgb.DMatrix(x_test)
+
+num_boost_rounds = 210
+model = xgb.train(dict(xgb_params, silent=1), dtrain, num_boost_round=num_boost_rounds)
+xgb_pred = model.predict(dtest)
+
+# Combine predictions
+xgb_weight = 1 - lgb_weight
+pred = xgb_weight*xgb_pred + lgb_weight*p_test
